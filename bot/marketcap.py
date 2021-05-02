fb07a8d87d49593c94c6eaa293b76884a62b1d2a
Manuel Silva <madusilva@gmail.com>
2021-05-02 17:20:00 +0100

    More logging

--- bot/marketcap.py
+++ bot/marketcap.py
@@ -20,15 +20,17 @@ def get_market_cap(api_key):
     session = Session()
     session.headers.update(headers)
     market_cap_info = {}
+    logger.debug('Requesting market caps using key: %s' % api_key)
     try:
         response = session.get(url, params=parameters)
         data = json.loads(response.text)
         for c in data.get('data', {}):
-            print(c.get('id'), c.get('name'), c.get('symbol'), c.get('cmc_rank'), c.keys())
+            # print(c.get('id'), c.get('name'), c.get('symbol'), c.get('cmc_rank'), c.keys())
             if c.get('symbol') is not None:
                 market_cap_info[c.get('symbol')] = {'rank': c.get('cmc_rank'), 'supply': c.get('circulating_supply')}
         #   print(data.get('data'))
     except (ConnectionError, Timeout, TooManyRedirects) as e:
+        logger.error('Could not retrieve market caps: %s' % e, exc_info=True)
         market_cap_info = {}
     return market_cap_info
 if __name__ == "__main__":
