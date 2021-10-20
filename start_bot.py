297e08b3105565f4dff499408c7e505d740d32be
Manuel Silva <madusilva@gmail.com>
2021-10-21 00:22:29 +0100

    Correct configuration file selection

--- start_bot.py
+++ start_bot.py
@@ -11,12 +11,12 @@ def main():
     parser.add_argument('env', nargs='?', default='test', type=str, help="Chose environment", choices=['production', 'test'])
     parser.add_argument('-c', '--configuration', nargs=1, default='configuration', type=str, help="Chose configration file")
     args = parser.parse_args()
-    logger.info("Starting bot in %s mode using %s configuration file" % (args.env, args.configuration[0]+'.json'))
+    logger.info("Starting bot in %s mode using %s configuration file" % (args.env, args.configuration+'.json'))
     # create console handler and set level to debug
     ch1 = logging.StreamHandler()
     ch1.setLevel(logging.DEBUG)
     #File logging
-    ch2 = logging.handlers.TimedRotatingFileHandler('robot_'+args.configuration[-1].split('_')[0]+'.log', when='D', interval=1, backupCount=5, delay=False, utc=True)
+    ch2 = logging.handlers.TimedRotatingFileHandler('robot_'+args.configuration.split('_')[0]+'.log', when='D', interval=1, backupCount=5, delay=False, utc=True)
     # create formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     # add formatter to ch
@@ -25,7 +25,7 @@ def main():
     # add ch to logger
     logger.addHandler(ch1)
     logger.addHandler(ch2)
-    bot.robot.run(args.env, configuration_file=args.configuration[0])
+    bot.robot.run(args.env, configuration_file=args.configuration)
 
 if __name__ == "__main__":
     main()
